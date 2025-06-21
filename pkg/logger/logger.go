14145a9f56dc725089787c9439c41976b3584edc
Minh Bui <btminh.dev@gmail.com>
2025-06-21 03:28:56 +0000

    Add request ID middleware and enhance logging with request ID support

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -20,6 +20,16 @@ func Error(msg string, err error, fields map[string]interface{}) {
 	logFields(fields).Err(err).Msg(msg)
 }
 
+func InfoWithRequestID(msg string, requestID string, fields map[string]interface{}) {
+	fields["request_id"] = requestID
+	Info(msg, fields)
+}
+
+func ErrorWithRequestID(msg string, err error, requestID string, fields map[string]interface{}) {
+	fields["request_id"] = requestID
+	Error(msg, err, fields)
+}
+
 func logFields(fields map[string]interface{}) *zerolog.Logger {
 	l := log.With()
 	for k, v := range fields {
