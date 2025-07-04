357f9f398f7e8e7d8ac9842ca2b89d6abe47a424
Tuấn Minh Bùi <btminh.dev@gmail.com>
2025-07-04 19:11:36 +0700

    feat: implement user registration functionality and related database schema (#13)

--- internal/initialize/di.go
+++ internal/initialize/di.go
@@ -2,22 +2,32 @@ package initialize
 
 import (
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/mingtmt/book-store/internal/books/application"
-	"github.com/mingtmt/book-store/internal/books/controller"
-	"github.com/mingtmt/book-store/internal/books/infrastructure/persistence"
+	bookService "github.com/mingtmt/book-store/internal/books/application"
+	bookController "github.com/mingtmt/book-store/internal/books/controller"
+	bookRepo "github.com/mingtmt/book-store/internal/books/infrastructure/persistence"
+	userService "github.com/mingtmt/book-store/internal/users/application"
+	userController "github.com/mingtmt/book-store/internal/users/controller"
+	userRepo "github.com/mingtmt/book-store/internal/users/infrastructure/persistence"
 )
 
 type Container struct {
-	BookHandler *controller.BookHandler
+	BookHandler *bookController.BookHandler
+	UserHandler *userController.UserHandler
 }
 
 func NewContainer(dbPool *pgxpool.Pool) *Container {
+	// Users
+	userRepo := userRepo.NewUserRepository(dbPool)
+	userService := userService.NewAuthService(userRepo)
+	userHandler := userController.NewUserHandler(userService)
+
 	// Books
-	bookRepo := persistence.NewBookRepository(dbPool)
-	bookService := application.NewBookService(bookRepo)
-	bookHandler := controller.NewBookHandler(bookService)
+	bookRepo := bookRepo.NewBookRepository(dbPool)
+	bookService := bookService.NewBookService(bookRepo)
+	bookHandler := bookController.NewBookHandler(bookService)
 
 	return &Container{
 		BookHandler: bookHandler,
+		UserHandler: userHandler,
 	}
 }
