572cc1fb44c30499dddae8c7990f0fb1333041d3
Minh Bui <btminh.dev@gmail.com>
2025-08-06 15:38:04 +0700

    setup albemic to create migration schema

--- app/main.py
+++ app/main.py
@@ -1,11 +1,8 @@
 from fastapi import FastAPI
 from app.api.v1.routes import ping
-from app.core.database import Base, engine
 
 app = FastAPI()
 
 # Mount route
 app.include_router(ping.router, prefix="/api/v1", tags=["Ping"])
-
-Base.metadata.create_all(bind=engine)
 
